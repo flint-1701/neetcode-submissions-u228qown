1cd545e79886b16035a690d171f18c7000ae8220
Akshay Birari <83076041+flint-1701@users.noreply.github.com>
2026-04-05 13:15:53 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int> temp = new Dictionary<int,int>();
+        for(int i=0;i<nums.Length;i++){
+            int comp = target - nums[i];
+            if(temp.ContainsKey(comp) && temp[comp]!=i) return new int[]{temp[comp],i};
+            else temp[nums[i]] = i;
+        }
+        return new int[]{0,0};
+    }
+}
